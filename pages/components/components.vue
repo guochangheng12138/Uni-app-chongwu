b8cb098bcbb6ba23a72e8117031b8578c027c26c
Guo Changheng <guochangheng@126.com>
2021-10-20 18:14:41 +0800

    5555

---
+++ pages/components/components.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		555555555555555
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
